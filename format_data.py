102fc539c7cdf20139f5ae2d2cd99013889fdf77
anna-deng <annadeng2020@u.northwestern.edu>
2020-05-04 16:30:53 -0700

    remove space from Data foldername and start script

--- format_data.py
+++ format_data.py
@@ -1,12 +1,22 @@
 import numpy as np
 import sys
+# from io import StringIO
 
 def main():
-    files_to_format = [] 
-    if(len(sys.argv) == 0):
-        # if no input is given, format all documents in data folder 
-    else: 
-        
+    # c = StringIO(sys.argv[1])
+
+    # files_to_format = [] 
+    # if(len(sys.argv) == 0):
+    #     # if no input is given, format all documents in data folder 
+    # else: 
+    separator = '--------------------------------------------------------------------------------'
+    text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, delimiter=separator, usecols=np.arange(0,1))
+    # text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, usecols=np.arange(0,1))
+    # text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, delimiter=separator, usecols=np.arange(0,1))
+    print('hello')
+    for x in range(text_array.size):
+        print(x, text_array[x])
+
 
 if __name__ == "__main__":
     main()
