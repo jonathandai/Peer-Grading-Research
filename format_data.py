27798bb350d7e156fda9ce3cac2232c7988f5ec5
Jonathan Dai <jonathandai@Jonathans-MacBook-Pro.local>
2020-05-03 23:41:45 -0700

    added data and python script skeleton

---
+++ format_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys
+
+def main():
+    files_to_format = [] 
+    if(len(sys.argv) == 0):
+        # if no input is given, format all documents in data folder 
+    else: 
+        
+
+if __name__ == "__main__":
+    main()
