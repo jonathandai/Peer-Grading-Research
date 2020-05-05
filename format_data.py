ed3ca95e11c46d58d3ab61aef4a952346d77c899
Jonathan Dai <jonathandai@Jonathans-MacBook-Pro.local>
2020-05-04 21:39:58 -0700

    added pandas data frame

--- format_data.py
+++ format_data.py
@@ -1,22 +1,31 @@
 import numpy as np
 import sys
-# from io import StringIO
+import pandas as pd 
 
 def main():
-    # c = StringIO(sys.argv[1])
-
     # files_to_format = [] 
     # if(len(sys.argv) == 0):
     #     # if no input is given, format all documents in data folder 
     # else: 
     separator = '--------------------------------------------------------------------------------'
+
+    # This is the right one 
+    ## text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, delimiter=separator, usecols=np.arange(0,1))
     text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, delimiter=separator, usecols=np.arange(0,1))
-    # text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, usecols=np.arange(0,1))
-    # text_array = np.genfromtxt(fname=sys.argv[1], dtype=str, delimiter=separator, usecols=np.arange(0,1))
-    print('hello')
     for x in range(text_array.size):
         print(x, text_array[x])
 
+    # format np matrix into proper excel format 
+    
+
+    # convert your array into a dataframe
+    df = pd.DataFrame(text_array)
+    # save to xlsx file
+    filepath = 'data.xlsx' 
+    df.to_excel(filepath, index=False)
 
 if __name__ == "__main__":
     main()
+
+
+
