c978a18ed23600ce3960e14fe8395a6e00b792ca
anna-deng <annadeng2020@u.northwestern.edu>
2020-05-05 16:41:14 -0700

    started parsing

--- format_data.py
+++ format_data.py
@@ -2,6 +2,13 @@ import numpy as np
 import sys
 import pandas as pd 
 
+# def parse_problem():
+
+
+def split_field(line):
+	split_data = line.split(': ', 1)
+	return split_data
+
 def main():
     # files_to_format = [] 
     # if(len(sys.argv) == 0):
@@ -19,8 +26,45 @@ def main():
 
 
     # format np matrix into proper excel format 
-    
+    titles = ['problem id', 'person type', 'person id', 'Algorithm', 'Proof', 'Clarity']
 
+    parsed_array = []
+    parsed_array[0] = titles
+
+	feedback_entry = []
+
+    # for line in text_array:
+    #     if line.contains('********************'):
+	# 		problem_id = get_field(line)
+	# 		feedback_entry[0] = problem_id
+	# 	elif line.contains('TA id'):
+	# 		person_type = 'TA'
+	# 		person_id = get_field(line)
+	# 		feedback_entry[1] = person_id
+	# 		feedback_entry[2] = person_type
+	# 	elif line.contains('peer id'):
+	# 		person_type = 'peer'
+	# 		person_id = get_field(line)
+	# 		feedback_entry[1] = person_id
+	# 		feedback_entry[2] = person_type
+	# 	elif line.contains('*Algorithm*'):
+	# 		algorithm_text = get_field(line)
+	# 		feedback_entry[3] = algorithm_text
+
+	i = 0
+	while i < text_array.size:
+		if text_array[i].contains('********************'):
+			problem_id = split_field(line)[1]
+			feedback_entry.push(problem_id)
+	    elif text_array[i] == '--------------------------------------------------':
+			i += 1
+			person_type = split_field(text_array[i])[0]
+			person_id = split_field(text_array[i])[1]
+			feedback_entry.push(person_type)
+			feedback_entry.push(person_id)
+			i += 1
+			while text_array[i+1] != '--------------------------------------------------':
+				
 
 
     # convert your array into a dataframe
